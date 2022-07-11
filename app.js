da9db6c3205490bcffd624e70308fad8219df4d1
shivam <shivamkhantwal9410@gmail.com>
2022-07-11 15:59:39 +0530

    Added

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@sandbox.vevuw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
     console.log("your server is running at port 5000")
   })
   .catch(err => {
