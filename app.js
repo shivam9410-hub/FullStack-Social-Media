30233d8420b182de42dc1648769b09c1b148705a
shivam khantwal <shivamkhantwal9410@gmail.com>
2023-05-01 16:27:21 +0530

    ...

--- app.js
+++ app.js
@@ -5,14 +5,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const placesRoutes = require('./routes/places-routes');
-const usersRoutes = require('./routes/users-routes');
-const HttpError = require('./models/http-error');
-
+const placesRoutes=require('./routes/places-routes');
+const usersRoutes=require('./routes/users-routes');
+const HttpError=require('./models/http-error');
 const app = express();
-
 app.use(bodyParser.json());
-
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
@@ -49,7 +46,8 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@sandbox.vevuw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }
+
+    `mongodb+srv://khantwal:${process.env.DB_PASSWORD}@sandbox.vevuw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
     app.listen(process.env.PORT || 5000);
